10839f3425ddf36b94fe72e60ca19c84a9e45f09
taikihomma <taikihomma0509@gmail.com>
2020-02-01 16:03:56 +0900

    create logtest.py

---
+++ logtest.py
@@ -0,0 +1,10 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+h = logging.FileHandler("logtest.log")
+logger.addHandler(h)
+
+def do_something():
+    logger.info("from logtest")
+    logger.debug("from logtest debug")
