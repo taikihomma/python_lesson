1250a7a1a1c275923d20249541d3fcbb02d1b771
taikihomma <taikihomma0509@gmail.com>
2020-02-01 15:44:09 +0900

    initial command

---
+++ lesson.py
@@ -0,0 +1,19 @@
+from optparse import OptionParser
+
+
+def main():
+    usage = "usage: %prog [options] arg1 arg2"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-f", "--file", action="store", type="string",
+                      dest="filename", help="File name")
+    options, args = parser.parse_args()
+    print(options.filename)
+    print(args)
+
+
+def sub_func():
+    pass
+
+
+if __name__ == "__main__":
+    main()
